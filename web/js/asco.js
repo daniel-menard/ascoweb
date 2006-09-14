bc078bd7c98f6d61922377e462baec2ec1ef56c0
Severine Ferron <severine.ferron@ehesp.fr>
2006-09-14 09:44:51 +0000

    Ajout de fonctions pour sélectionner/désélectionner les notices dans une liste de réponses.
    git-svn-id: http://ascoweb.googlecode.com/svn/trunk@3 0621ce33-663a-0410-bb36-b14c205f76dc

--- web/js/asco.js
+++ web/js/asco.js
@@ -1,8 +1,8 @@
 function selectAll(value)
 {
-     var f = document.getElementById("answers");
+     var f = ElementById("answers");
      var nb = f.length ;
-     if (f[0]==null) alert("Desole, mais votre navigateur ne supporte pas cette fonction...") ;
+     if (f[0]==null) alert("Désoleé mais votre navigateur ne supporte pas cette fonction...") ;
      for (var i=0;i<nb;i++)
      {
 		var e = f[i] ;
@@ -10,3 +10,11 @@ function selectAll(value)
 		if (e.name == "art") e.checked = value ;
      }
 };
+
+function ElementById(id)
+{
+	if (document.getElementById(id)) return document.getElementById(id);
+	if (document.all[id]) return document.all[id];
+	if (document.layers && document.layers[id]) return document.layers[id];
+	return null;
+}
