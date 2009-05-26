1db4399acfd7d8ec81e2f0bc0831272a59e57a79
Daniel Menard <daniel.menard@laposte.net>
2009-05-26 13:34:48 +0000

    SpipSecurity : nettoyage du code utilisé.
    git-svn-id: http://ascoweb.googlecode.com/svn/trunk@606 0621ce33-663a-0410-bb36-b14c205f76dc

--- modules/SpipSecurity/SpipSecurity.php
+++ modules/SpipSecurity/SpipSecurity.php
@@ -8,17 +8,17 @@
 
 /**
  * Module de sécurité.
- * 
- * Ce module gère la connexion des utilisateurs SPIP depuis le site web 
+ *
+ * Ce module gère la connexion des utilisateurs SPIP depuis le site web
  * d'Ascodocpsy.
- * 
+ *
  * On distingue trois types d'utilisateurs :
  * - le grand public
  * - les membres du GIP
  * - les administrateurs
- * 
+ *
  * @package     ascoweb
- * @subpackage  SpipSecurity 
+ * @subpackage  SpipSecurity
  */
 class SpipSecurity extends NoSecurity
 {
@@ -28,21 +28,21 @@ class SpipSecurity extends NoSecurity
      * @var string
      */
     public $ident='';
-    
+
     /**
      * Login de la personne connectée
      *
      * @var string
      */
     public $login='';
-    
+
     /**
      * Email de la personne connectée
      *
      * @var string
      */
     public $email='';
-    
+
     public function __construct()
     {
         if ($h=Utils::get($_COOKIE['user']))
@@ -56,7 +56,7 @@ class SpipSecurity extends NoSecurity
                 $this->rights=$t['rights'];
             }
         }
-    }	
+    }
 
     /**
      * Connecte un utilisateur identifié à partir du site Ascodocpsy sous SPIP et
@@ -64,35 +64,22 @@ class SpipSecurity extends NoSecurity
      *
      * On distingue trois types d'utilisateurs :
      * - le grand public : pas de droit particulier
-     * - les membres du GIP : droit "Edit" 
+     * - les membres du GIP : droit "Edit"
      * - les administrateurs : droit "Admin"
      */
     public function actionConnect()
     {
-        // on attend en querystring une chaine qui est la version base64 de la sérialisation du tableau suivant :
-        // $t=array (
-        //   'ident' => 'Asco1',
-        //   'login' => 'asco1',
-        //   'email' => 'nadine.carrasco@ch-montperrin.fr',
-        //   'statut' => '6forum',
-        // );
-        // Version sérialisée du tableau :
-        // a:4:{s:5:"ident";s:5:"Asco1";s:5:"login";s:5:"asco1";s:5:"email";s:32:"nadine.carrasco@ch-montperrin.fr";s:6:"statut";s:6:"6forum";}
-        // Version encodée en base 64 :
-        // YTo0OntzOjU6ImlkZW50IjtzOjU6IkFzY28xIjtzOjU6ImxvZ2luIjtzOjU6ImFzY28xIjtzOjU6ImVtYWlsIjtzOjMyOiJuYWRpbmUuY2FycmFzY29AY2gtbW9udHBlcnJpbi5mciI7czo2OiJzdGF0dXQiO3M6NjoiNmZvcnVtIjt9
-
-
-        // Décode et désérialise la query string qu'on nous a passée
+        // Décode la query string qu'on nous a passée
         $request=unserialize(base64_decode($_SERVER['QUERY_STRING']));
 
-        // Récupère l'ident, le login, l'email tel quel                
+        // Récupère l'ident, le login, l'email tel quel
         $t=array
         (
             'ident'=>Utils::get($request['ident']),
             'login'=>Utils::get($request['login']),
             'email'=>Utils::get($request['email']),
         );
-        
+
         // Détermine les droits en fonction du statut spip
         switch(Utils::get($request['statut']))
         {
@@ -102,20 +89,17 @@ class SpipSecurity extends NoSecurity
             case '0minirezo': // administrateur
                 $t['rights']='Admin';
                 break;
-//            case '1comite': // rédacteur, non utilisé
-//                $rights='';
-//                break;
             default:
                 $t['rights']='';
                 break;
         }
-         
+
         // Crée un cookie 'user' contenant la version base64 de $t
         setcookie('user', base64_encode(serialize($t)), 0, '/');
-        
+
         // Détermine l'url de redirection
         $url=Utils::get($request['url'], '/Base/SearchForm');
-        
+
         Runtime::redirect($url);
     }
 }
