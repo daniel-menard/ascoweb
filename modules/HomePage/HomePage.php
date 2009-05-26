cca90af249451e3c822f4b672ae08f32c0e3f08b
Daniel Menard <daniel.menard@laposte.net>
2009-05-26 13:38:49 +0000

    HomePage : remplacement du mot de passe "poivron" qui était codé en dur dans le code du module par un mot de passe indiqué dans le fichier general.config.
    git-svn-id: http://ascoweb.googlecode.com/svn/trunk@608 0621ce33-663a-0410-bb36-b14c205f76dc

--- modules/HomePage/HomePage.php
+++ modules/HomePage/HomePage.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * Created on 31 mai 06
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
 class HomePage extends Module
 {
     public function preExecute()
@@ -16,20 +10,23 @@ class HomePage extends Module
                 break;
             case 'actionIndex':
                 $password=Utils::get($_POST['password']);
-                if ($password=='poivron')
+                if (!empty($password) && ($password===Config::get('mainpassword')))
+                {
                     Runtime::redirect('/HomePage/Home');
+                    return;
+                }
                 $this->setLayout('default.htm');
                 break;
             default: die(__METHOD__.' : action non reconnue <pre>'.print_r($this,true));
         }
-            
+
     }
-    
+
 	public function actionHome()
     {
     	// jamais appellé parce que le layout 'demo' ne contient pas de balise 'contents'
     }
-    
+
     public function actionIndex()
     {
         Template::run('login.html');
